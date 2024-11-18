04b6080848593fc53f0cc72c208e444324c9823f
Alberto Meneghini <menalb84@gmail.com>
2024-11-18 09:22:11 +0100

    init

---
+++ src/SentinelProject.API/Features/ProcessTransaction.cs
@@ -0,0 +1,27 @@
+﻿using FastEndpoints;
+using FluentValidation;
+using Microsoft.AspNetCore.Authorization;
+
+namespace SentinelProject.API.Features;
+
+public record AnalyzeTransactionRequest(Guid TransactionId, Guid UserId, decimal Amount, string Location, string Merchant, string Device, string TransactionType);
+
+public record AnalyzeTransactionResponse(string ResponseUrl);
+
+[HttpPost("transactions")]
+[AllowAnonymous]
+public class ProcessTransaction: Endpoint<AnalyzeTransactionRequest, AnalyzeTransactionResponse>
+{
+    public override Task<AnalyzeTransactionResponse> ExecuteAsync(AnalyzeTransactionRequest req, CancellationToken ct)
+    {
+        return base.ExecuteAsync(req, ct);
+    }
+}
+
+public class AnalyzeTransactionValidator : Validator<AnalyzeTransactionRequest>
+{
+    public AnalyzeTransactionValidator()
+    {
+        RuleFor(t => t.Amount).GreaterThan(0);
+    }
+}
---
+++ src/SentinelProject.Tests/Validators/RequestValidator.cs
@@ -0,0 +1,37 @@
+﻿using SentinelProject.API.Features;
+
+namespace SentinelProject.Tests.Validators;
+
+public class AnalyzeTransactionValidatorTests
+{
+    private readonly AnalyzeTransactionValidator validator = new();
+
+    [Theory(DisplayName = "Invalid Transaction Amount")]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void InvalidTransactionAmount(decimal amount)
+    {
+        // Arrange
+        var request = new AnalyzeTransactionRequest(Guid.NewGuid(), Guid.NewGuid(), amount, "Rome", "Merchant1", "Desktop", "Online");
+
+        // Act
+        var result = validator.Validate(request);
+
+        // Assert
+        Assert.False(result.IsValid);
+        Assert.Contains(result.Errors, e => e.PropertyName == nameof(request.Amount));
+    }
+
+    [Fact(DisplayName ="Valid Transaction")]
+    public void ValidTransaction()
+    {
+        // Arrange
+        var request = new AnalyzeTransactionRequest(Guid.NewGuid(), Guid.NewGuid(), 15, "Rome", "Merchant1", "Desktop", "Online");
+
+        // Act
+        var result = validator.Validate(request);
+
+        // Assert
+        Assert.True(result.IsValid);
+    }
+}
